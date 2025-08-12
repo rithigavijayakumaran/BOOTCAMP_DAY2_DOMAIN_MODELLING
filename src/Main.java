b7e6219f89450ba5f46828ea7740bc56eccf18b2
Rithiga <rithiga2026@gmail.com>
2025-08-12 23:54:27 +0530

    Your message here

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        SwiggyPlatform platform = new SwiggyPlatform();
         }
-
 }
 
 class SwiggyPlatform{
